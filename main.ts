2af76e493eff604e9177d57c5c991023be81a49c
JosephBarker123 <Wagg14@vp.local>
2016-12-01 14:45:36 +0000

    Initial Upload
    The first upload of the binary display package

---
+++ main.ts
@@ -0,0 +1,32 @@
+/**
+*function that blinks
+*/
+//% weight=5 color=#2699BF
+namespace Counter {
+	/**
+	 * Displays the inputted number as binary on the LEDs, where the top left LED is 0 or 1 and the bottom right LED is the highest possible value (rows, then columns)
+	 *@param power is the power of 2 when calculating the binary number eg. 0, 1, 24
+	 **/
+	//% blockId=block_play
+	//% help=display-binary
+	//% block="show binary of|number %decimal"
+	export function displayBinary(decimal: number): void{
+		let power = 24
+		basic.clearScreen()
+		if (decimal <= 33554431) {
+			while (decimal > 0) {
+				if (decimal >= Math.pow(2, power)) {
+					decimal += -Math.pow(2, power)
+					for (let i = 0; i <= 4; i++) {
+						if (power <= 5 * (i + 1) - 1) {
+							led.plot(power - 5 * i, i)
+						}
+					}
+				}
+				power += -1
+			}
+		} else {
+			basic.showString("Too Big")
+		}
+	}
+}
